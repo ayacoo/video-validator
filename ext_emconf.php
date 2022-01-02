b984c29a7c6c151f097f047c31d5e0ef4092d421
Guido Schmechel <guido@ayacoo.de>
2022-01-02 20:00:33 +0100

    [TASK] Fix version

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,14 +2,14 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Video validator',
-    'description' => 'Checks online videos for availability',
+    'description' => 'Checks online videos in TYPO3 for availability',
     'category' => 'plugin',
     'author' => 'Guido Schmechel',
     'author_email' => 'info@ayacoo.de',
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.0',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.13-11.9.99',
