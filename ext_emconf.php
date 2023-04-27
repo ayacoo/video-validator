78626aa93ac52be83f667e587e64b465dcabc862
Guido Schmechel <guido@ayacoo.de>
2023-04-27 23:43:41 +0200

    [TASK] Change composer dependencies and version

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.0.0',
+    'version' => '3.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.9.99',
+            'typo3' => '12.4.0-12.9.99',
         ],
         'conflicts' => [
         ],
