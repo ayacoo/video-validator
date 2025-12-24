b47d28612ba443954ac36991bb10f11a81e17073
Guido Schmechel <guido@ayacoo.de>
2025-12-24 14:27:46 +0100

    [TASK] Add PHP 8.5 support

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '4.1.2',
+    'version' => '4.1.3',
     'constraints' => [
         'depends' => [
-            'php' => '8.2.0-8.4.99',
+            'php' => '8.2.0-8.5.99',
             'typo3' => '13.0.0-13.4.99',
         ],
         'conflicts' => [
